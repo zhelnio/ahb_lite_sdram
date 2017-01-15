0cf5327cbab9adfd28fb6e85594a675f4a21cc1f
Stanislav Zhelnio <zhelniosl@yandex.ru>
2017-01-15 23:12:19 +0300

    tcl script bugfix

--- run/03_AhbLiteSdram.tcl
+++ run/03_AhbLiteSdram.tcl
@@ -11,10 +11,11 @@ set a6 +incdir+../../src/ahb_lite_sdram
 # set a6 +incdir+../../src/testbench/sdr_sdram/sdr_parameters.vh
 set a7 +incdir+../../src/testbench/sdr_sdram/sdv.v
 set a8 ../../src/testbench/sdr_sdram/*.v
+set a10 ../../src/ahb_lite_sdram/*.v
 # set a8 ../../src/testbench/sdr_sdram/sdv.v
 set a9 ../../src/testbench/test_ahb_lite_sdram.v
 
-vlog $a0 $a1 $a2 $a3 $a4 $a5 $a6 $a7 $a8 $a9
+vlog $a0 $a1 $a2 $a3 $a4 $a5 $a6 $a7 $a8 $a10 $a9
 #  
 
 vsim work.test_ahb_lite_sdram
