49488a645b7d1116b98a4c226111c922ab0f6f5e
Stanislav Zhelnio <zhelniosl@yandex.ru>
2017-01-24 19:54:26 +0300

    small change for prev commit

--- run/01_AhbLiteMem.tcl
+++ run/01_AhbLiteMem.tcl
@@ -1,17 +1,23 @@
 
 vlib work
 
-set a0 -vlog01compat
-set a1 +define+den512Mb
-set a2 +define+sg75
-set a3 +define+x16
-set a4 +define+SIMULATION
+set p0 -vlog01compat
+set p1 +define+den512Mb
+set p2 +define+sg75
+set p3 +define+x16
+set p4 +define+SIMULATION
 
-set a5 +incdir+../../src/testbench/sdr_sdram
-set a6 +incdir+../../src/ahb_lite_sdram
-set a8 ../../src/testbench/test_ahb_lite_mem.v
+set i0 +incdir+../../src/ahb_lite_sdram
+set i1 +incdir+../../src/testbench
+set i2 +incdir+../../src/testbench/sdr_sdram
+set i3 +incdir+../../src/debug
 
-vlog $a0 $a1 $a2 $a3 $a4 $a5 $a6  $a8 
+set s0 ../../src/ahb_lite_sdram/*.v
+set s1 ../../src/testbench/*.v
+set s2 ../../src/testbench/sdr_sdram/*.v
+set s3 ../../src/debug/*.v
+
+vlog $p0 $p1 $p2 $p3 $p4  $i0 $i1 $i2 $i3  $s0 $s1 $s2 $s3
 
 vsim work.test_ahb_lite_mem
 add wave -radix hex sim:/test_ahb_lite_mem/*
